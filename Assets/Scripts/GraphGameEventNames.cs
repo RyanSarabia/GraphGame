2fe3341efe90f4e8afdf5e6085dd755449f4c20b
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-07-25 04:04:04 +0800

    added direction strings

--- Assets/Scripts/GraphGameEventNames.cs
+++ Assets/Scripts/GraphGameEventNames.cs
@@ -9,4 +9,8 @@ public class GraphGameEventNames
     public const string SOUTH = "SOUTH";
     public const string EAST = "EAST";
     public const string WEST = "WEST";
+    public const string LOCAL_NORTH = "LOCAL_NORTH";
+    public const string LOCAL_SOUTH = "LOCAL_SOUTH";
+    public const string LOCAL_EAST = "LOCAL_EAST";
+    public const string LOCAL_WEST = "LOCAL_WEST";
 }
