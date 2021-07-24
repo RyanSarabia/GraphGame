205a4d98fdd07becd442ad9f466ec9887d2a8975
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-07-25 03:33:09 +0800

    changed room prefabs and added direction strings to EventNames

--- Assets/Scripts/GraphGameEventNames.cs
+++ Assets/Scripts/GraphGameEventNames.cs
@@ -5,4 +5,8 @@ using UnityEngine;
 public class GraphGameEventNames
 {
     public const string LIGHT_UP = "LIGHT_UP";
+    public const string NORTH = "NORTH";
+    public const string SOUTH = "SOUTH";
+    public const string EAST = "EAST";
+    public const string WEST = "WEST";
 }
